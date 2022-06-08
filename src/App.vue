7dcf0a9457a318364521fa9fe4f690353be2073b
Suhyxn <momentomori21489@gmail.com>
2022-06-08 15:37:40 +0900

    Start
    기본 ToDo 설정

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="todo">
+    <div class="form-group">
+      <h1>
+        <span class="vue">Vue</span> To-Do
+        <small>List</small>
+      </h1>
+      <form role="form">
+        <input
+          v-model="title"
+          type="text"
+          class="form-control"
+          placeholder="해야 할 일" />
+      </form>
+      <button
+        type="button"
+        class="btn btn btn-primary"
+        @click="addToDo(title)">
+        Add
+      </button>
+    </div>
+    <div class="todo-list">
+      <ToDoList
+        :todos="todos"
+        @del-todo="delToDo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ToDoList from '~/components/ToDoList.vue'
+export default {
+  components: {
+    ToDoList
+  },
+  data() {
+    return {
+      title: '',
+      todos: [
+        {
+          title: '운동하기'
+        },
+        {
+          title: 'JAVA'
+        },
+        {
+          title: 'javascript'
+        }
+      ]
+    }
+  },
+  methods: {
+    addToDo(title) {
+      if(title) {
+        this.todos.push({title})
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.todoApp {
+  margin: auto;
+  width: 500px;
+}
+form {
+    display: inline-block;
+}
+.form-group {
+    text-align: center;
+    margin-top: 10em;
+    padding-bottom: 25px;
+}
+.todo-list {
+    width: 250px;
+    text-align: left;
+    margin-left: 130px;
+}
+a.close {
+    float: right;
+}
+.vue {
+    color: #009900;
+}
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul
+      v-for="(todo,i) in todos"
+      :key="todo.id"
+      class="list-unstyled">
+      <li>
+        {{ i+1 }}. {{ todo.title }} <span
+          class="close"
+          aria-hidden="true">&times;</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      default: undefined
+    },
+    methods: {
+      delToDo (todo) {
+        this.$emit('del-todo', todo)
+      }
+    }
+  }
+}
+</script>
